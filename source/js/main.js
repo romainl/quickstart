b0e29e96fc4d04e6ecb5e5b0547cdd4ec960e9a9
Romain Lafourcade <romain.lafourcade@fullsix.com>
2016-11-13 01:14:31 +0100

    comments

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,6 @@
-/*
- * js/main.js
- */
-var caca;
-console.log("caca", caca);
-var foo = require("./modules/foo");
+// js/main.js
+// ==========
+
 (function main() {
 	// do stuff
-	console.log(foo, foo, 13);
 })();
